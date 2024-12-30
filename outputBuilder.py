1fd6410a7205144c768ce38d3a988c812829217a
Phil <91299055+pkonkle213@users.noreply.github.com>
2024-12-30 19:15:25 +0000

    Removed dead code

--- outputBuilder.py
+++ outputBuilder.py
@@ -1,53 +1,6 @@
 import datetime
 import pytz
 
-def PrintCommands(canTrack,
-                  canSubmit,
-                  isPhil,
-                  isMyGuild,
-                  isStoreOwner,
-                  isCBUSMTG):
-  responses = []
-  isPhil = False
-
-  responses.append('Here are my current commands:')
-
-  if isMyGuild:
-    responses.append('`$bot` - Displays the link to this bot')
-    responses.append('`$sheets` - Displays the link to the Google Sheets companion')
-
-    if False and isPhil:
-      responses.append('`$approveStore` `storeDiscordId` - Approves a store to track')
-      responses.append('`$revokeApproval` `storeDiscordId` - Takes away a store\'s approval to track')
-      responses.append('`$getAllStores` - Displays all stores in the database')
-
-    if isStoreOwner:
-      responses.append('`$addSubmitter` `user_id` - Adds a user to your allowed submitter list')
-      responses.append('`$deleteSubmitter` `user_id` - removes a user from the submitter list')
-      responses.append('`$myEvents` - View your store\'s events')
-      responses.append('`$seeEvent` ~`date`~`game`~`format` - View the players in an event')
-      responses.append('`$topPlayers` `format` - Displays the top players for a given format in the current quarter year')
-      responses.append('`$topPlayers` `format` `startDate` `endDate` - Displays the top players for a given format in the given date range')
-      responses.append('`$updateDataRow` followed by a list of properly formatted strings will update a datarow')
-
-  elif isCBUSMTG:
-    responses.append('Soon to come!')
-    #responses.append('`$metagame` `storeName` - Displays the metagame for the format channel you\'re in for the past 8 weeks')
-  else:
-    if isPhil:
-      responses.append('`$test` - Sends details to me concerning the discord guild, channel, category, etc')
-
-    if not canTrack:
-      responses.append('`$register` `storeName` - Register a store to track and an owner for the store')
-    else:
-      responses.append('`$metagame` - Prints a table for the channel\'s metagame')
-      responses.append('`$metagame` `startDate` `endDate` - Similar but for a date range')
-      if canSubmit:
-        responses.append('`$addResults` followed by a list of properly formatted strings will add event data to the database')
-
-  response = '\n'.join(responses)
-  return response
-
 def MaxLength(headers, collection):
   buffer = 2
   maxLengths = []
