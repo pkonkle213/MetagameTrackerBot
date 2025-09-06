7bc1ef32db4bdd3f31516857e1d809cdf91e5dc9
Phil <91299055+pkonkle213@users.noreply.github.com>
2025-09-06 21:49:01 +0000

    Stores that remove the bot from the discord are now removed from the database. This keeps all their events and archetypes though

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ CMDS_DIR = BASE_DIR / "commands"
 @bot.event
 async def on_ready():
   print(f'Logged on as {format(bot.user)}!')
-  scheduled_post.start()
+  data_guild_update.start()
   update_store_levels.start()
   await SyncCommands(bot, CMDS_DIR)
 
@@ -38,7 +38,7 @@ async def before_update_store_levels():
   await bot.wait_until_ready()
 
 @tasks.loop(time=datetime.time(hour=14, minute=00, tzinfo=datetime.timezone.utc)) 
-async def scheduled_post():
+async def data_guild_update():
   """Every Friday at 10:00 AM EST, the data guild is updated with new data"""
   time_now = datetime.datetime.now(datetime.timezone.utc)
   if time_now.weekday() == 4:  # Check if it's Friday, 0 = Monday
@@ -47,7 +47,7 @@ async def scheduled_post():
     except Exception as error:
       print(f'Error updating data guild: {error}')
 
-@scheduled_post.before_loop
+@data_guild_update.before_loop
 async def before_scheduled_post():
   await bot.wait_until_ready()
 
