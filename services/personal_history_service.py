28e3da1ba830e19a55bbdc0635817f247cffffd4
Phil <91299055+pkonkle213@users.noreply.github.com>
2025-09-06 14:21:27 +0000

    Wrapped up personal history. Still thinking about mixing with personal group

--- services/personal_history_service.py
+++ services/personal_history_service.py
@@ -1,6 +1,6 @@
 import discord
 from interaction_objects import GetObjectsFromInteraction
-from data.personal_history_data import GetStandingsHistory
+from data.personal_history_data import GetStandingsHistory, GetPairingsHistory
 from output_builder import BuildTableOutput
 from services.date_functions import BuildDateRange
 
@@ -10,12 +10,32 @@ def GetPersonalStandingsHistory(interaction: discord.Interaction, start_date: st
   date_start, date_end = BuildDateRange(start_date, end_date, format)
   data = GetStandingsHistory(user_id, game, format, date_start, date_end, store)
   if data is None:
-    return None
+    return 'No history found.'
   title = f'Personal History for {store.StoreName.title()}'
   headers = ['Date', 'Archetype', 'Wins', 'Losses', 'Draws']
   if not format:
     headers.insert(1, 'Format')
   if not game:
     headers.insert(1, 'Game')
+  if not store:
+    headers.insert(1, 'Store Name')
+  output = BuildTableOutput(title, headers, data)
+  return output
+
+def GetPersonalPairingsHistory(interaction: discord.Interaction, start_date: str, end_date: str):
+  """Gets the personal pairings history for the user"""
+  game, format, store, user_id = GetObjectsFromInteraction(interaction, store=True)
+  date_start, date_end = BuildDateRange(start_date, end_date, format)
+  data = GetPairingsHistory(user_id, game, format, date_start, date_end, store)
+  if data is None:
+    return 'No history found.'
+  title = f'Personal History for {store.StoreName.title()}'
+  headers = ['Date', 'Round', 'Archetype', 'Result']
+  if not format:
+    headers.insert(1, 'Format')
+  if not game:
+    headers.insert(1, 'Game')
+  if not store:
+    headers.insert(1, 'Store Name')
   output = BuildTableOutput(title, headers, data)
   return output
