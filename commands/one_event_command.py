7443cd624a3cf7a072db2400a775e7ee17cfd1c3
Phil <91299055+pkonkle213@users.noreply.github.com>
2026-02-27 23:19:04 +0000

    Added metagame command to One Event

--- commands/one_event_command.py
+++ commands/one_event_command.py
@@ -4,19 +4,34 @@ from discord import app_commands, Interaction
 from discord.ext import commands
 from input_modals.event_selector import EventSelector
 from services.command_error_service import Error
-from services.claim_result_services import OneEvent
+from services.claim_result_services import OneEventDetails, OneEventMeta
 from output_builder import BuildTableOutput
 
 class OneEventCommands(commands.GroupCog, name='oneevent'):
   def __init__(self, bot):
     self.bot = bot
 
-  @app_commands.command(name='store',
-    description='Download the store data for a date range')
+  @app_commands.command(name='metagame',
+    description='View the metagame for one event')
   @app_commands.guild_only()
   @app_commands.checks.cooldown(1, 60.0, key=lambda i: (i.guild_id, i.user.id))
   async def OneEventMeta(self, interaction:Interaction):
-    await interaction.response.defer(ephemeral=True)
+    store, game, format = GetObjectsFromInteraction(interaction)
+    if not store or not game or not format:
+      raise KnownError('No store, game, or format found.')
+
+    modal = EventSelector(store, game, format)
+    await interaction.response.send_modal(modal)
+    await modal.wait()
+
+    if not modal.is_submitted:
+      raise Exception('Modal was not submitted')
+
+    event = modal.event
+
+    title, headers, data = OneEventMeta(event)
+    output = BuildTableOutput(title, headers, data)
+    await interaction.followup.send(output)    
   
   @app_commands.command(name="topresults",
                        description="Get the archetypes for an event and their results")
@@ -37,7 +52,7 @@ class OneEventCommands(commands.GroupCog, name='oneevent'):
 
     event = modal.event
 
-    title, headers, data = OneEvent(event)
+    title, headers, data = OneEventDetails(event)
     output = BuildTableOutput(title, headers, data)
 
     await interaction.followup.send(output)
