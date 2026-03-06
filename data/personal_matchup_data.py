55f480fbfb810cf103dae37880d8dd0a466a6f6b
Phil <91299055+pkonkle213@users.noreply.github.com>
2026-03-06 17:32:28 +0000

    Secret update to matchup sql

--- data/personal_matchup_data.py
+++ data/personal_matchup_data.py
@@ -16,11 +16,11 @@ def GetPersonalMatchups(
   with conn, conn.cursor() as cur:
     command = f'''
     SELECT
-      INITCAP(COALESCE(ua.archetype_played, 'UNKNOWN')) AS archetype_played,
+      COALESCE(INITCAP(ua.archetype_played), 'UNKNOWN') AS archetype_played,
+      COUNT(*) as total_matches,
       COUNT(CASE WHEN result = 'WIN' THEN 1 END) as wins,
       COUNT(CASE WHEN result = 'LOSS' THEN 1 END) as losses,
       COUNT(CASE WHEN result = 'DRAW' THEN 1 END) as draws,
-      COUNT(*) as total_matches,
       ROUND(1.0 * COUNT(CASE WHEN result = 'WIN' THEN 1 END) / (COUNT(CASE WHEN result = 'WIN' THEN 1 END) + COUNT(CASE WHEN result = 'LOSS' THEN 1 END) + COUNT(CASE WHEN result = 'DRAW' THEN 1 END)) * 100, 2) as win_percent
     FROM
       full_pairings fp
@@ -37,10 +37,41 @@ def GetPersonalMatchups(
       AND e.format_id = {format.FormatId}
       AND e.event_date BETWEEN '{start_date}' AND '{end_date}'
     GROUP BY
-      INITCAP(COALESCE(ua.archetype_played, 'UNKNOWN'))
-    ORDER BY COUNT(*) DESC, 
-      INITCAP(COALESCE(ua.archetype_played, 'UNKNOWN'))
+      INITCAP(ua.archetype_played)
+    ORDER BY
+      2 DESC,
+      1
     '''
+
+"""
+SELECT
+  COALESCE(INITCAP(ua.archetype_played), 'UNKNOWN') AS archetype_played,
+  COUNT(*) as total_matches,
+  COUNT(CASE WHEN result = 'WIN' THEN 1 END) as wins,
+  COUNT(CASE WHEN result = 'LOSS' THEN 1 END) as losses,
+  COUNT(CASE WHEN result = 'DRAW' THEN 1 END) as draws,
+  ROUND(1.0 * COUNT(CASE WHEN result = 'WIN' THEN 1 END) / (COUNT(CASE WHEN result = 'WIN' THEN 1 END) + COUNT(CASE WHEN result = 'LOSS' THEN 1 END) + COUNT(CASE WHEN result = 'DRAW' THEN 1 END)) * 100, 2) as win_percent
+FROM
+  full_pairings fp
+  INNER JOIN events e ON fp.event_id = e.id
+  LEFT JOIN unique_archetypes ua ON UPPER(fp.opponent_name) = UPPER(ua.player_name)
+    AND ua.event_id = fp.event_id
+  INNER JOIN player_names pn ON pn.discord_id = e.discord_id
+    AND UPPER(fp.player_name) = UPPER(pn.player_name)
+WHERE
+  pn.submitter_id = 153591222148136970
+  AND UPPER(fp.opponent_name) != 'BYE'
+  AND e.discord_id = 1210746744602890310
+  AND e.game_id = 1
+  AND e.format_id = 1
+  AND e.event_date >= '1/1/2026'
+GROUP BY
+  INITCAP(ua.archetype_played)
+ORDER BY
+  2 DESC,
+  1
+"""
+    
     
     cur.execute(command)
     rows = cur.fetchall()
