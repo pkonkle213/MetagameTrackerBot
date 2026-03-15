a312d4320673a786c18f92765f63ce8566af52f0
Phil <91299055+pkonkle213@users.noreply.github.com>
2026-03-15 17:15:35 +0000

    Set up with two example decklists. Pseudo coding for now before presenting an actual command to test

---
+++ _nonfunctioning/convert_decklist/decklist_converter.py
@@ -0,0 +1,13 @@
+"""
+My thoughts on how to convert a decklist:
+  1. Get the decklist from the user
+  2. Convert the decklist to a format that can be used by the bot
+  3. Retrieve the rules for archetypes
+  4. Go through each card (name?) and see what the deck could be    
+"""
+
+"""
+Things needed:
+  1. I need to make a database of rules. What archetype names are, what cards define them, and what cards specifically differ them from similar archetypes.
+  This has been done before (https://github.com/Badaro/MTGOArchetypeParser), but I need to put my spin on it, specifically for pauper.
+"""
