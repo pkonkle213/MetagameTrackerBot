006871040e7a63fec5876fdeb3a8490c89163cc3
Phil <91299055+pkonkle213@users.noreply.github.com>
2026-03-15 17:22:21 +0000

    Continuing talking to myself

--- _nonfunctioning/convert_decklist/decklist_converter.py
+++ _nonfunctioning/convert_decklist/decklist_converter.py
@@ -3,11 +3,19 @@ My thoughts on how to convert a decklist:
   1. Get the decklist from the user
   2. Convert the decklist to a format that can be used by the bot
   3. Retrieve the rules for archetypes
-  4. Go through each card (name?) and see what the deck could be    
+  4. Go through each card (name?) and see what the deck could be
+  5. After determining the archetype, save the decklist to the database linked to the archetype submission by ID (which I just removed, great)
 """
 
 """
 Things needed:
   1. I need to make a database of rules. What archetype names are, what cards define them, and what cards specifically differ them from similar archetypes.
   This has been done before (https://github.com/Badaro/MTGOArchetypeParser), but I need to put my spin on it, specifically for pauper.
+  2. I need to give archetype submissions an ID again, as a decklist will be linked to it
+  3. I'll need more tables to store cards and decklists.
+    3.1. One table for cards (ID, Name) - This will eliminate redundant entries in my table (lots of islands!)
+    3.2. One table for decklists (ArchetypeSubmissionID, CardID, Quantity)
+  4. Better examples of decks to test against
+    4.1. I need at least two distinct archetypes that are easy to tell from each other (bogles vs spy)
+    4.2. I need at least two distinct archetypes that are hard to tell from each other (White Weenie vs Boros Bully, Elves vs Spy, Mono Red Madness vs Rakdos Madness, Mono Blue Terror vs Dimir Terror, Mono Blue Faeries vs Dimir Faeries vs Izzet Faeries)
 """
