14158f28bff0cb78f07c3f6d3454308b7a27144e
Phil <91299055+pkonkle213@users.noreply.github.com>
2026-03-21 17:42:34 +0000

    Bug fix for Top Players

--- services/top_players_services.py
+++ services/top_players_services.py
@@ -11,5 +11,5 @@ def GetTopPlayers(interaction: Interaction, start_date, end_date):
   date_start, date_end = BuildDateRange(start_date, end_date, format)
   data = GetTopPlayerData(store, game, format, date_start, date_end)
   title = f'Top Players from {date_start.strftime('%B %d')} to {date_end.strftime('%B %d')}'
-  headers = ['Name', 'Total Points', 'Win %']
+  headers = ['Name', 'Points', 'Win %']
   return data, title, headers
