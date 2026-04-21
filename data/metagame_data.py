8c6a3ff9cc4dad06c00d119f8fee490bae4f94ba
Phil <91299055+pkonkle213@users.noreply.github.com>
2026-04-21 22:35:55 +0000

    temp fix

--- data/metagame_data.py
+++ data/metagame_data.py
@@ -9,13 +9,6 @@ from tuple_conversions import Event, Format, Game, Store, League, MetagameResult
 def GetWhere(event:Event) -> str:
   return f'AND e.league_id = {event.league_id}'
 
-def GetAreaForMeta(store: Store) -> str:
-  if store.discord_id == settings.DATAGUILDID:
-    return f'AND s.used_for_data = {True}'
-  if store.is_data_hub:
-    return f'AND s.region_id = {store.region_id}'
-  return f'AND s.discord_id = {store.discord_id}'
-
 #TODO: Can I make 1 metagame method for simplicity? Add a CTE at the end to control what rows it looks at, then the rest is standard/repeated
 def GetLeagueMetagame(
   league:League
@@ -118,7 +111,7 @@ def GetMetagame(
         INNER JOIN stores s ON e.discord_id = s.discord_id
       WHERE
         e.event_date BETWEEN '{start_date}' AND '{end_date}'
-        {GetAreaForMeta(store)}
+        AND s.discord_id = {store.discord_id}
         AND e.format_id = {format.id}
         AND e.game_id = {game.id}
       GROUP BY
