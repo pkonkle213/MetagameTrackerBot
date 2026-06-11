f66eb47a1e71c3f396e3d70d85bda6b7e4a96225
Phil <91299055+pkonkle213@users.noreply.github.com>
2026-06-11 18:27:09 +0000

    Quick fix and more thoughts

--- services/event_details_services.py
+++ services/event_details_services.py
@@ -5,7 +5,7 @@ from tuple_conversions import OutputToBuild
 def GetEventStats(discord_id:int) -> OutputToBuild:
   data = GetAllEventsStats(discord_id)
   title = 'Event Statistics'
-  headers = ['Event Date', 'Game', 'Format','Attended', 'Users','Users Percent', 'Archetypes', 'Archetypes Percent']
+  headers = ['Event Date', 'Game', 'Format','Attended', 'Archetypes','Archetypes Percent', 'Distinct Users', 'Users Percent']
   if not discord_id:
     headers.insert(1, 'Store')
   return OutputToBuild(title, headers, data)
