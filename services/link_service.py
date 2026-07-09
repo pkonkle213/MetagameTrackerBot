6d09e8089e68006344114032b668fb77f22ff876
Phil <91299055+pkonkle213@users.noreply.github.com>
2026-07-09 23:15:40 +0000

    Duplicate service

--- services/link_service.py
+++
@@ -1,7 +0,0 @@
-from interaction_objects import GetObjectsFromInteraction
-from discord import Interaction
-
-def GetAllHubsInvites(interaction: Interaction) -> None:
-  objects = GetObjectsFromInteraction(interaction)
-  
-  ...
