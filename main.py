8a2b70258d214d3fcf61961e51339f2bb916936f
Phil <91299055+pkonkle213@users.noreply.github.com>
2026-08-09 03:52:36 +0000

    Stupid bug on installation. Not the best look

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from services.store_services import NewStoreRegistration
 from timedposts.automated_updates import UpdateDataGuild
 from services.sync_service import SyncCommands
 from discord_messages import MessageUser
+from timedposts.automated_paid_users import UpdateStores
 # from services.stripe_webhook_service import start_webhook_server
 
 intents = discord.Intents.all()
