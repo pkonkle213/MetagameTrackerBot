4db36f7fbdedfa974cf31b9445442fb137f9fb11
Phil <91299055+pkonkle213@users.noreply.github.com>
2026-08-09 03:35:21 +0000

    Updating command to be used in stores

--- commands/event_details_command.py
+++ commands/event_details_command.py
@@ -1,3 +1,4 @@
+from interaction_objects import GetObjectsFromInteraction
 from discord import Interaction, app_commands
 from discord.ext import commands
 
@@ -6,30 +7,26 @@ from output_builder import BuildTableOutput
 from services.command_error_service import Error
 from services.event_details_services import GetEventStats
 from settings import BOTGUILDID
+from checks import IsStore
 
 
 class UniqueSubmitters(commands.Cog):
-    def __init__(self, bot:commands.Bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @app_commands.command(
         name="events_statistics",
         description="See unique submitters and percent reported for all events",
     )
-    @app_commands.guilds(BOTGUILDID)
-    @app_commands.check(isPhil)
-    async def MyEventsReported(self, interaction: Interaction, discord_id: str = ""):
-        """
-        Parameters:
-        -----------
-        discord_id: string
-          The discord id of the store to check
-        """
+    @app_commands.checks.has_role("MTSubmitter")
+    @app_commands.guild_only()
+    @IsStore()
+    async def MyEventsReported(self, interaction: Interaction):
         await interaction.response.defer(thinking=True)
-        discord_id_int = 0
-        if discord_id != "":
-            discord_id_int = int(discord_id)
-        table = GetEventStats(discord_id_int)
+        objects = GetObjectsFromInteraction(interaction)
+        if not objects.store or not objects.game or not objects.format:
+            raise Exception("No store, game, or format found.")
+        table = GetEventStats(objects.store, objects.game, objects.format)
         output = BuildTableOutput(table.title, table.headers, table.data)
         await interaction.followup.send(output)
 
@@ -40,5 +37,5 @@ class UniqueSubmitters(commands.Cog):
         await Error(self.bot, interaction, error)
 
 
-async def setup(bot:commands.Bot):
+async def setup(bot: commands.Bot):
     await bot.add_cog(UniqueSubmitters(bot))
